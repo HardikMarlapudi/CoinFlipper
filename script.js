33b6e9383f2e95c59e12f318a7cf2e77289264b2
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-10-13 00:22:42 -0400

    added the coin image to be switched from heads or tails

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("flipCoin");
-document.getElementById("reset");
+document.getElementById("flipCoin").addEventListener(onclick, flipCoin);
 document.getElementById("result");
 document.getElementById("headScore");
 document.getElementById("tailsScore");
+document.getElementById("coinImage");
 document.getElementById("coin-flipper");
 
 let heads = 0;
@@ -10,15 +10,18 @@ let tails = 0;
 
 function flipCoin() {
     let coin = Math.floor(Math.random() * 2);
+    let coinImage = document.getElementById("coin-flipper");
 
     if(coin === 0) {
         heads +=1;
         document.getElementById("result").innerHTML = "You got heads.";
         document.getElementById("headScore").innerText = `Heads: ${heads}`;
+        coinImage.src = "head.png";
     } else {
         tails +=1;
         document.getElementById("result").innerHTML = "You got tails.";
-        document.getElementById("tailsScore").innerText = `Tails: ${tails}`
+        document.getElementById("tailsScore").innerText = `Tails: ${tails}`;
+        coinImage.src = "tail.png";
     }
 }
 
@@ -28,4 +31,5 @@ function resetBtn() {
     document.getElementById("headScore").innerText = `Heads: ${heads}`;
     document.getElementById("tailsScore").innerText = `Tails: ${tails}`;
     document.getElementById("result").innerHTML = `Result: `;
+    document.getElementById("coin-flipper").src = "head.png";
 }
